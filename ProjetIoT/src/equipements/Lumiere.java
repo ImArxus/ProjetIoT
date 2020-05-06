7a25fe48c3373581b6d8c2e5feeedb6e7bbb2ecd
mezianekhodja <meziane.khodja@etudiant.univ-rennes1.fr>
2020-05-06 15:19:43 +0200

    changement couleurs classe Lumiere

--- ProjetIoT/src/equipements/Lumiere.java
+++ ProjetIoT/src/equipements/Lumiere.java
@@ -1,24 +1,30 @@
 package equipements;
 
+import java.util.Scanner;
+
 import main.Equipement;
 
 public class Lumiere extends Equipement {
 
 	private int intensite;
+	private String couleur;
 
 	public Lumiere(String nom) {
 		super(nom, false);
 		setIntensite(100);
+		couleur = "blanc";
 	}
 
-	public Lumiere(String nom, boolean etatCourant, int intensite) {
+	public Lumiere(String nom, boolean etatCourant, int intensite, String couleur) {
 		super(nom, etatCourant);
 		setIntensite(intensite);
+		setCouleur(couleur);
 	}
 
 	@Override
 	public String actionsPossibles() {
-		return super.actionsPossibles() + "\n-> Augmenter intensité\n-> Diminuer intensité\n-> Choisir intensité";
+		return super.actionsPossibles()
+				+ "\n-> Augmenter intensité\n-> Diminuer intensité\n-> Choisir intensité\n-> Choisir couleur";
 	}
 
 	public int getIntensite() {
@@ -28,11 +34,35 @@ public class Lumiere extends Equipement {
 	public void setIntensite(int intensite) {
 		this.intensite = intensite;
 	}
-	
+
+	public String getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(String couleur) {
+		if ((couleur.equals("blanc")) || (couleur.equals("bleu")) || (couleur.equals("rouge"))
+				|| (couleur.equals("jaune")) || (couleur.equals("vert"))) {
+			this.couleur = couleur;
+		} else {
+			this.couleur = "blanc";
+		}
+	}
+
+	public void changerCouleur() {
+		if (super.isEtatCourant()) {
+			System.out.println("Veuillez entrer la couleur choisie parmis les suivantes : blanc bleu rouge jaune vert");
+			Scanner s = new Scanner(System.in);
+			String requete = s.nextLine();
+			setCouleur(requete);
+		} else {
+			System.out.println(this.getNom() + " est éteinte, on ne peut pas changer la couleur");
+		}
+	}
+
 	public void augmenterIntensite() {
 		if (super.isEtatCourant()) {
 			if (getIntensite() < 100) {
-				intensite+=10;
+				intensite += 10;
 			}
 		} else {
 			System.out.println(this.getNom() + " est éteinte, on ne peut pas augmenter l'intensité");
@@ -42,7 +72,7 @@ public class Lumiere extends Equipement {
 	public void diminuerIntensite() {
 		if (super.isEtatCourant()) {
 			if (getIntensite() > 0) {
-				intensite-=10;
+				intensite -= 10;
 			}
 		} else {
 			System.out.println(this.getNom() + " est éteinte, on ne peut pas diminuer l'intensité");
