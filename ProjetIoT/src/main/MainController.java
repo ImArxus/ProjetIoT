6be38f24af067a3acb32a2cc28d59c3950c44dbf
ImArxus <alex.lebalanger@icloud.com>
2020-05-20 14:30:52 +0200

    Test ChoixMaison

--- ProjetIoT/src/main/MainController.java
+++ ProjetIoT/src/main/MainController.java
@@ -35,7 +35,7 @@ public class MainController {
 	private TextField newUserTxt;
 	@FXML
 	private PasswordField newPasswordTxt;
-	
+
 	/**
 	 * Champs page Choix maison
 	 */
@@ -55,11 +55,22 @@ public class MainController {
 		if (listeUtilisateur.comptes.containsKey(userTxt.getText())
 				&& listeUtilisateur.comptes.get(userTxt.getText()).equals(passwordTxt.getText())) {
 			loginTxt.setText("Bienvenue " + userTxt.getText() + " !");
-			Thread.sleep(2000);
 			versChoixMaison(event);
 		} else {
 			loginTxt.setText("Identifiant ou mot de passe incorrect");
 		}
+	}
+
+	public void versMaisonBarry(ActionEvent event) {
+		choixTxt.setText("Bienvenue dans la maison de Barry !");
+	}
+
+	public void versMaisonVide(ActionEvent event) {
+		choixTxt.setText("Votre maison de rêve n'attend que vous !");
+	}
+
+	public void versMaisonChargee(ActionEvent event) {
+		choixTxt.setText("Votre maison est chargée !");
 	}
 
 	public void versChoixMaison(ActionEvent event) {
@@ -88,13 +99,15 @@ public class MainController {
 
 	public void creerCompte() {
 		if (!listeUtilisateur.comptes.containsKey(newUserTxt.getText())) {
-			if (!newPasswordTxt.getText().isEmpty()) {
+			if (newUserTxt.getText().isEmpty()) {
+				creationTxt.setText("Veuillez entrer un identifiant");
+			} else if (newPasswordTxt.getText().isEmpty()) {
+				creationTxt.setText("Veuillez entrer un mot de passe");
+			} else {
 				listeUtilisateur.comptes.put(newUserTxt.getText(), newPasswordTxt.getText());
 				ListeUtilisateurs.getAdmin().put(newUserTxt.getText(), false);
 				Sauvegarde.sauvegarderCompte();
 				creationTxt.setText("Féliciations, vous avez maintenant un compte utilisateur !");
-			} else {
-				creationTxt.setText("Veuillez entrer un mot de passe");
 			}
 		} else {
 			creationTxt.setText("Identifiant déjà existant");
