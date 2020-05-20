e206a4a7f267a66290c96eb57b10b0d388b262b9
ImArxus <alex.lebalanger@icloud.com>
2020-05-20 16:57:46 +0200

    Affichage des pièces vides dans chaque maison

--- ProjetIoT/src/main/MainController.java
+++ ProjetIoT/src/main/MainController.java
@@ -58,6 +58,7 @@ public class MainController {
 	public void login(ActionEvent event) throws InterruptedException {
 		if (listeUtilisateur.comptes.containsKey(userTxt.getText())
 				&& listeUtilisateur.comptes.get(userTxt.getText()).equals(passwordTxt.getText())) {
+			Main.setPseudo(userTxt.getText());
 			loginTxt.setText("Bienvenue " + userTxt.getText() + " !");
 			versChoixMaison(event);
 		} else {
@@ -72,10 +73,9 @@ public class MainController {
 		Scene scene = new Scene(root);
 		
 		ImageView imageView = new ImageView();
-        imageView.setImage(new Image("/images/vide.png"));
-        imageView.setLayoutY(100);
+        imageView.setImage(new Image("/images/piece.png"));
         imageView.setFitWidth(800);
-        imageView.setFitHeight(500);
+        imageView.setFitHeight(600);
 
         root.getChildren().add(imageView);
         
@@ -86,10 +86,38 @@ public class MainController {
 
 	public void versMaisonVide(ActionEvent event) {
 		choixTxt.setText("Votre maison de rêve n'attend que vous !");
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Pane root = (Pane) getRoot("/main/Maison.fxml");
+		Scene scene = new Scene(root);
+		
+		ImageView imageView = new ImageView();
+        imageView.setImage(new Image("/images/piece.png"));
+        imageView.setFitWidth(800);
+        imageView.setFitHeight(600);
+
+        root.getChildren().add(imageView);
+        
+		window.setTitle("Maison vide");
+		window.setScene(scene);
+		window.show();
 	}
 
 	public void versMaisonChargee(ActionEvent event) {
 		choixTxt.setText("Votre maison est chargée !");
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Pane root = (Pane) getRoot("/main/Maison.fxml");
+		Scene scene = new Scene(root);
+		
+		ImageView imageView = new ImageView();
+        imageView.setImage(new Image("/images/piece.png"));
+        imageView.setFitWidth(800);
+        imageView.setFitHeight(600);
+
+        root.getChildren().add(imageView);
+        
+		window.setTitle("Maison de " + Main.getPseudo());
+		window.setScene(scene);
+		window.show();
 	}
 
 	public void versChoixMaison(ActionEvent event) {
